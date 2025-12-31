f50bedb2c5da1343f5e7e5fc370a28489370aeed
23f3004177 <0x30n3055@gmail.com>
2025-12-31 23:35:38 +0530

    Test Drive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use polars::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    let mut df:DataFrame = df!(
+        "name" => ["Alice Archer","Ben Brown"],
+        "weight" => [57.9,72.5]
+    ).unwrap();
+    println!("df:{}",df.clone().lazy().select([col("name")]).collect().unwrap());
 }
